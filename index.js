fc80daa0e4204fcd9f199d6e2612a464c50d8dec
prajwal p prabhu <prajwalprabhu025@gmail.com>
2024-02-04 21:40:01 +0530

    second commit

--- index.js
+++ index.js
@@ -2,14 +2,47 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
-
+const githubdata={
+    "login": "hiteshchoudhary",
+    "id": 11613311,
+    "node_id": "MDQ6VXNlcjExNjEzMzEx",
+    "avatar_url": "https://avatars.githubusercontent.com/u/11613311?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/hiteshchoudhary",
+    "html_url": "https://github.com/hiteshchoudhary",
+    "followers_url": "https://api.github.com/users/hiteshchoudhary/followers",
+    "following_url": "https://api.github.com/users/hiteshchoudhary/following{/other_user}",
+    "gists_url": "https://api.github.com/users/hiteshchoudhary/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/hiteshchoudhary/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/hiteshchoudhary/subscriptions",
+    "organizations_url": "https://api.github.com/users/hiteshchoudhary/orgs",
+    "repos_url": "https://api.github.com/users/hiteshchoudhary/repos",
+    "events_url": "https://api.github.com/users/hiteshchoudhary/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/hiteshchoudhary/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Hitesh Choudhary",
+    "company": null,
+    "blog": "https://www.youtube.com/c/HiteshChoudharydotcom",
+    "location": "India",
+    "email": null,
+    "hireable": null,
+    "bio": "I make coding videos on youtube and for courses. My youtube channel explains my work more. Check that out",
+    "twitter_username": "hiteshdotcom",
+    "public_repos": 79,
+    "public_gists": 2,
+    "followers": 14520,
+    "following": 0,
+    "created_at": "2015-03-23T13:03:25Z",
+    "updated_at": "2023-12-31T10:07:11Z"
+  }
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.get('/twitter',(req,res)=>{res.send('xolu')})
 app.get('/login',(req,res)=>{res.send('zolu')})
 app.get('/youtube',(req,res)=>{res.send('zzzl')})
-
+app.get('/github',(req,res)=>{res.json(githubdata)})
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`)
 })
