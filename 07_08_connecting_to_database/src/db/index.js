321f30572522445e287e46253f17ff7c12f1b8b0
prajwal p prabhu <prajwalprabhu025@gmail.com>
2024-02-16 01:33:41 +0530

    building profeessional code with database connection

---
+++ 07_08_connecting_to_database/src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "../constants.js";
+const connectDB =async () =>{
+    try{
+        const connectionInstance=await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+console.log(`\nMongodb connected DBhost DBhost: ${connectionInstance.connection.host}`)
+
+    }catch(error){
+        console.log("Mongodbconnetionerror",error);
+        process.exit(1)
+    }
+
+
+}
+export default connectDB 
