a49cd1de405f07a43d87b5fecc0be1f4d9a4dffd
prajwal p prabhu <prajwalprabhu025@gmail.com>
2024-02-25 12:10:03 +0530

    utils fucntions video 9

--- 07_08_connecting_to_database/src/index.js
+++ 07_08_connecting_to_database/src/index.js
@@ -8,6 +8,15 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>
+{app.listen(process.env.PORT||8000,()=>{
+    
+    console.log(`server  is runign at port:${process.env.PORT}`);
+})})
+.catch((err)=>{
+    console.log("mongo db connectionfailed!!!",err);
+
+})
 
 
 
---
+++ 07_08_connecting_to_database/src/utils/asyncHandler.js
@@ -0,0 +1,18 @@
+const asyncHandler=(requesthandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
+}
+export {asyncHandler}
+// const asyncHandler=()=>{}
+//  const asyncHandler=(func)=>async()=>{}
+// const asyncHandler=(func)=>async(req,res,next)=>{
+//     try{await func(req,res,next)}
+//     catch(error){
+//         res.status(err.code||500).json({
+//             success:false,
+//             message:err.message
+//         })
+//     }
+// } 
+
